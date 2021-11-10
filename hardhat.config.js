a649c6a64e46ef86fbd6be7ab908272f2045682e
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2021-11-10 17:12:11 -0500

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
-
+require("@nomiclabs/hardhat-etherscan")
 // Go to https://www.alchemyapi.io, sign up, create
 // a new App in its dashboard, and replace "KEY" with its key
 
@@ -11,12 +11,14 @@ const PRIVATE_KEY = process.env.devTestnetPrivateKey
 const RINKEBY_RPC_URL = process.env.rinkebyInfuraAPIKey
 
 module.exports = {
-  solidity: "0.8.9",
   networks: {
     rinkeby: {
       url: RINKEBY_RPC_URL,
       accounts: [`0x${process.env.devTestnetPrivateKey}`]
     }
-  }
+  },
+    etherscan: {
+    apiKey: process.env.etherscanAPIKey
+  },
+  solidity: "0.8.9"
 };
-
