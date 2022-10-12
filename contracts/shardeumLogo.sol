605b3179736f6d5ed088ce650d50dcf7829c0d7a
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2022-10-12 10:50:08 -0400

    Update shardeumLogo.sol

--- contracts/shardeumLogo.sol
+++ contracts/shardeumLogo.sol
@@ -5,9 +5,9 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract ERC721_Example is ERC721URIStorage {
 
-    constructor() ERC721 ("shardeumLogo", "SL"){
-        _safeMint(msg.sender, 1);
-        _setTokenURI(1, "https://ipfs.fleek.co/ipfs/bafybeib6zcl5v5ojxkvmxnvpqrypq5yakmu2fd6y6wc3xo4n66pjelu7yq");
-    }
+  constructor() ERC721 ("shardeumLogo", "SL"){
+    _safeMint(msg.sender, 1);
+    _setTokenURI(1, "bafybeib6zcl5v5ojxkvmxnvpqrypq5yakmu2fd6y6wc3xo4n66pjelu7yq"); //IPFS CID
+  }
 
 }
