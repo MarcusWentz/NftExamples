f7238015ba4ec937551b28d812e2e38de44dc313
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2022-10-18 10:01:55 -0400

    Update fruitStore.sol

--- contracts/ERC-1155/fruitStore.sol
+++ contracts/ERC-1155/fruitStore.sol
@@ -3,9 +3,11 @@ pragma solidity 0.8.17;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
+error invalidFruitIdNumber(); 
+
 contract FruitStore is ERC1155 {
 
-    uint public constant Apple = 0; //"constant" can never change. "immutable" can only be changed in the constructor.
+    uint public constant Apple = 0; //"constant" can never change. 
     uint public constant Banana = 1;
     uint public constant Pitaya = 2;
     uint public constant Guava = 3;
@@ -13,13 +15,19 @@ contract FruitStore is ERC1155 {
     uint public constant Raspberry = 5;
     uint public constant Blueberry = 6;
     uint public constant Avacado = 7;
+    uint public immutable ExtraFruitId; //"immutable" can only be changed in the constructor.
 
-    constructor() public ERC1155("https://en.wikipedia.org/wiki/{name}") {
-        _mint(msg.sender, Apple,      100*(1 ether), "");
-        _mint(msg.sender, Banana,     100*(1 ether), "");
-        _mint(msg.sender, Pitaya,     100*(1 ether), "");
-        _mint(msg.sender, Guava,      100*(1 ether), "");
-        _mint(msg.sender, Watermelon, 100*(1 ether), "");
-        _mint(msg.sender, Watermelon, 100*(1 ether), "");
+    constructor(uint extraFruitIdPick) public ERC1155("https://en.wikipedia.org/wiki/{name}") {
+        if(extraFruitIdPick > 7) { revert invalidFruitIdNumber(); } //Don't check for negative since uint must be 0 or positive.
+        ExtraFruitId = extraFruitIdPick; //"immutable" can only be changed in the constructor.
+        _mint(msg.sender, Apple,        100*(1 ether), "");
+        _mint(msg.sender, Banana,       100*(1 ether), "");
+        _mint(msg.sender, Pitaya,       100*(1 ether), "");
+        _mint(msg.sender, Guava,        100*(1 ether), "");
+        _mint(msg.sender, Watermelon,   100*(1 ether), "");
+        _mint(msg.sender, Raspberry,    100*(1 ether), "");
+        _mint(msg.sender, Blueberry,    100*(1 ether), "");
+        _mint(msg.sender, Avacado,      100*(1 ether), "");
+        _mint(msg.sender, ExtraFruitId, 100*(1 ether), "");
     }
 }
