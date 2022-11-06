87ab2736d0e9c0ad4dfba54fa02c86827069b099
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2022-11-06 10:34:08 -0500

    Update shardeumLogo.sol

--- contracts/ERC-721/shardeumLogo.sol
+++ contracts/ERC-721/shardeumLogo.sol
@@ -2,6 +2,9 @@
 pragma solidity 0.8.17;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+// import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+error onlyOwner();
 
 contract ShardeumLogoNFT is ERC721URIStorage {
 
@@ -10,4 +13,20 @@ contract ShardeumLogoNFT is ERC721URIStorage {
     _setTokenURI(1, "bafybeib6zcl5v5ojxkvmxnvpqrypq5yakmu2fd6y6wc3xo4n66pjelu7yq"); //For tokenId = 1, set IPFS CID for image data.
   }
 
+}
+
+contract SendAndRecoverERC721 is ERC721URIStorage  { 
+
+    address public immutable Owner;
+    ShardeumLogoNFT public immutable instance;
+
+    constructor(address deployedAddress) ERC721 ("shardeumLogo", "SL") {
+        Owner = msg.sender;
+        instance = ShardeumLogoNFT(deployedAddress);
+    }
+
+    function sendBackTokenToWallet(uint tokenId) public {
+        if(msg.sender != Owner) { revert onlyOwner(); }
+        instance.transferFrom(address(this),msg.sender,tokenId);
+    }
 }
